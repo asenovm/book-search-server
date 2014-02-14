7f2dccda47fd3d4c8ba8cb23896f4ee41a95830d
asenovm <asenov.m@gmail.com>
2014-02-14 02:42:27 +0200

    * Returning mock data from the search service

--- services/server.js
+++ services/server.js
@@ -1,8 +1,32 @@
 var express = require('express'),
     app = express();
 
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+   res.header('Access-Control-Allow-Credentials', true);
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   next();
+});
+
 app.get('/search', function (req, res) {
-    //TODO start java app using lucene with req.query.q
+    res.json({
+        "books": [
+            {
+                "title": "Sherlock Holmes",
+                "image": "http://example.com/image.png",
+                "relevance": "0.77"
+            }, {
+                "title": "Withering Heights",
+                "image": "http://example.com/image2.png",
+                "relevance": "0.54"
+            }, {
+                "title": "The Picture of Dorian Gray",
+                "image": "http://example.com/image3.png",
+                "relevance": "0.36"
+            }   
+        ]
+    });
     res.send(200);
 });
 
